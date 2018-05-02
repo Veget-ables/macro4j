7d4e823918a1192b04ab4b5985bb46797e02d58c
TakakiTojo <makuhari109@gmail.com>
2018-05-02 16:27:41 +0900

    ランダãパングラムを にソートする

---
+++ src/main/java/PrintWord.java
@@ -0,0 +1,28 @@
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class PrintWord {
+
+    static void createWord(String absPath, Workbook book){
+        try (FileOutputStream out = new FileOutputStream(new File(absPath))) {
+            XWPFDocument document = addPangramList(book);
+            document.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static XWPFDocument addPangramList(Workbook book){
+        XWPFDocument document = new XWPFDocument();
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
+        run.setText("サンプル");
+        return document;
+    }
+}
--- src/main/java/SortMacro.java
+++ src/main/java/SortMacro.java
@@ -6,35 +6,43 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class SortMacro {
+    private static final String INPUT_DIR = "/Users/Tojo/Desktop/files/";
+    private static final String OUTPUT_DIR = "/Users/Tojo/Desktop/files/";
+
     private static Sheet mSheet;
     private static final int RANDOM_CELL = 10;
 
-    public static void main(String[] args) throws IOException, InvalidFormatException {
+    public static void main(String[] args) {
+        for (int i = 1; i < 13; i++) {
+            try {
+                String fileName = "pangram";
+                Workbook book = randomSortXSLX(INPUT_DIR + fileName + ".xlsx");
+                FileOutputStream fileOut = new FileOutputStream(OUTPUT_DIR + fileName + i + ".xlsx");
+                book.write(fileOut);
+//                PrintWord.createWord(OUTPUT_DIR + "test.docx", book);
+            } catch (IOException | InvalidFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-//        String inputPath = args[0];
-//        String outputPath = args[1];
-        String inputPath = "/Users/Tojo/Desktop/files/test.xlsx";
-        String outputPath = "/Users/Tojo/Desktop/files/test2.xlsx";
-
-        Workbook book = WorkbookFactory.create(new File(inputPath));
+    static private Workbook randomSortXSLX(String fileName) throws IOException, InvalidFormatException {
+        Workbook book = WorkbookFactory.create(new File(fileName));
         mSheet = book.getSheetAt(0);
 
         int startNextRow = 0;
         int currentRow = 0;
         while (true) {
             currentRow = sortSet(startNextRow, currentRow);
-
             if (mSheet.getRow(currentRow + 2) == null) break; // 空行が2行続く場合，データがないとみなし終了．
             currentRow++;
             startNextRow = currentRow;
         }
-
-        FileOutputStream fileOut = new FileOutputStream(outputPath);
-        book.write(fileOut);
+        return book;
     }
 
     // 1セットずつソートする．セットの終わりは改行で区別する．
-    private static int sortSet(int startRow, int currentRow){
+    private static int sortSet(int startRow, int currentRow) {
         while (true) {
             Row row = mSheet.getRow(currentRow);
             if (row == null) break; // 空行
@@ -56,10 +64,25 @@ public class SortMacro {
 
                 cellKana = mSheet.getRow(value).createCell(RANDOM_CELL);
                 cellKana.setCellValue(kana);
-                Cell cellKanji = mSheet.getRow(value).createCell(RANDOM_CELL +1);
+                Cell cellKanji = mSheet.getRow(value).createCell(RANDOM_CELL + 1);
                 cellKanji.setCellValue(kanji);
                 break;
             }
+        }
+        removeOldCells(startRow, setMax);
+    }
+
+    // ソート後の値を古い列に上書きする．
+    private static void removeOldCells(int startRow, int setMax){
+        for (int i = startRow; i < setMax; i++) {
+            Row row = mSheet.getRow(i);
+            Cell kanaCell = row.getCell(RANDOM_CELL);
+            Cell kanjiCell = row.getCell(RANDOM_CELL + 1);
+            row.getCell(1).setCellValue(kanaCell.getStringCellValue());
+            row.getCell(2).setCellValue(kanjiCell.getStringCellValue());
+
+//            kanaCell.removeCellComment();
+//            kanjiCell.removeCellComment();
         }
     }
 }
