ecb900cf8c4e186175a9b12171d21062ccec296a
TakakiTojo <makuhari109@gmail.com>
2018-05-03 21:55:34 +0900

    gitignoreを追加

--- src/main/java/PrintWord.java
+++ src/main/java/PrintWord.java
@@ -1,14 +1,7 @@
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTPageMargins;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBody;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDocument1;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
+import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -29,32 +22,59 @@ class PrintWord {
     private static XWPFDocument addPangramList(Workbook book) {
         Sheet sheet = book.getSheetAt(0);
         XWPFDocument document = new XWPFDocument();
-
         int wordId = 1;
-        int rowNum =0;
+        int rowNum = 0;
+        int day = 1;
+        insertDayTitle(document, day);
+
         while (true) {
-            XWPFParagraph paragraph = document.createParagraph();
-            XWPFRun run = paragraph.createRun();
+            XWPFRun run = document.createParagraph().createRun();
             Row row = sheet.getRow(rowNum);
             if (row == null) return document;
 
             String kana = row.getCell(1).getStringCellValue();
             String kanji = row.getCell(2).getStringCellValue();
-            String text;
-            if (wordId < 10) {
-                text = String.format("%s.    %s (%s) \n", wordId, kana, kanji);
-            } else {
-                text = String.format("%s.  %s (%s) \n", wordId, kana, kanji);
-            }
+
+            String format = wordId < 10 ? "%s.    %s (%s) \n" : "%s.  %s (%s) \n";
+            String text = String.format(format, wordId, kana, kanji);
             run.setText(text);
 
-            if(wordId == 18){
+            if (wordId == 18) {
+                document.createParagraph().createRun().addBreak();
+                insertTable(document);
                 document.createParagraph().setPageBreak(true);
+                insertDayTitle(document, ++day);
                 wordId = 1;
-            }else{
+            } else {
                 wordId++;
             }
             rowNum++;
         }
+    }
+
+    private static void insertDayTitle(XWPFDocument document, int day) {
+        XWPFParagraph paragraph = document.createParagraph();
+        paragraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun runTitle = paragraph.createRun();
+        runTitle.setFontSize(24);
+        runTitle.setText("計測" + day + "日目");
+    }
+
+    private static void insertTable(XWPFDocument document){
+        XWPFTable table = document.createTable(2, 1);
+        XWPFTableCell timeCell = table.getRow(0).getCell(0);
+        setCellWidth(timeCell);
+        XWPFParagraph timeParagraph = timeCell.getParagraphs().get(0);
+        XWPFRun run = timeParagraph.createRun();
+        run.setText("開始時刻:");
+
+        XWPFTableRow row = table.getRow(1);
+        row.setHeight(2000);
+        XWPFTableCell commentCell = row.getCell(0);
+        setCellWidth(commentCell);
+    }
+
+    private static void setCellWidth(XWPFTableCell cell){
+        cell.getCTTc().addNewTcPr().addNewTcW().setW(BigInteger.valueOf(10500L));
     }
 }
