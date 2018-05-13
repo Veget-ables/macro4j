d44e467374e59afe16b2c6e84d93b17be0b902a1
TakakiTojo <makuhari109@gmail.com>
2018-05-13 09:55:28 +0100

    KanaCounterの相対頻度を算出する

--- src/main/java/pangram/analyze/appearance/kana/KanaCountMacro.java
+++ src/main/java/pangram/analyze/appearance/kana/KanaCountMacro.java
@@ -7,37 +7,46 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class KanaCountMacro {
-    private static String FILE_PATH;
+    private static String INPUT_FILE_PATH;
+    private static String OUTPUT_FILE_PATH;
+    private static final int KANA_CELL = 20;
+    private static final int KANA_COUNT_CELL = 21;
+    private static final int KANA_APPEARANCE_RATE_CELL = 22;
 
     static {
-        FILE_PATH = System.getenv("TARGET_FILE_PATH"); // 読み込むファイルを設定する
+        INPUT_FILE_PATH = System.getenv("INPUT_FILE_PATH");
+        OUTPUT_FILE_PATH = System.getenv("OUTPUT_FILE_PATH");
     }
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
-        Workbook book = WorkbookFactory.create(new File(FILE_PATH));
-        Sheet mSheet = book.getSheetAt(0);
-
-        Map<Character, Integer> kanaMap = new HashMap<>();
-        int rowNum = 1;
-        while (true) {
-            Row row = mSheet.getRow(rowNum);
-            if (row == null) break;
-            String text = row.getCell(1).getStringCellValue();
-            for (Character kana : text.toCharArray()) {
-                if (kanaMap.containsKey(kana)) {
-                    int count = kanaMap.get(kana) + 1;
-                    kanaMap.put(kana, count);
-                } else {
-                    kanaMap.put(kana, 0);
-                }
-            }
-            rowNum++;
-        }
+        Workbook book = WorkbookFactory.create(new File(INPUT_FILE_PATH));
+        Sheet sheet = book.getSheetAt(0);
+        Map<String, Integer> kanaMap = KanaCounter.execute(sheet);
         kanaMap.forEach((k, v) -> System.out.println(k + " :" + v));
+
+        // 出現割合を算出するために，合計文字数を計算
+        int countAll =0;
+        for(String key : kanaMap.keySet()){
+            if(key.equals("、") || key.equals("。"))continue;
+            countAll += kanaMap.get(key);
+        }
+
+        int rowNum=1;
+        for(String key : kanaMap.keySet()){
+            Row row = sheet.getRow(rowNum++);
+            row.createCell(KANA_CELL).setCellValue(key);
+
+            int count = kanaMap.get(key);
+            row.createCell(KANA_COUNT_CELL).setCellValue(count);
+            row.createCell(KANA_APPEARANCE_RATE_CELL).setCellValue((double) count/countAll);
+        }
+
+        FileOutputStream fileOut = new FileOutputStream(OUTPUT_FILE_PATH);
+        book.write(fileOut);
     }
 }
--- src/main/java/pangram/analyze/appearance/kana/KanaCounter.java
+++ src/main/java/pangram/analyze/appearance/kana/KanaCounter.java
@@ -1,4 +1,69 @@
 package pangram.analyze.appearance.kana;
 
-public class KanaCounter {
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class KanaCounter {
+
+    static Map<String, Integer> execute(Sheet sheet) {
+        Map<String, Integer> charMap = countCharMap(sheet);
+        return countKanaMap(charMap);
+    }
+
+    private static Map<String, Integer> countCharMap(Sheet sheet){
+        Map<String, Integer> countMap = new HashMap<>();
+        int rowNum = 1;
+        while (true) {
+            Row row = sheet.getRow(rowNum);
+            if (row == null) break;
+            String text = row.getCell(1).getStringCellValue();
+            for (char kana : text.toCharArray()) {
+                String k = String.valueOf(kana);
+                if (countMap.containsKey(k)) {
+                    int count = countMap.get(k) + 1;
+                    countMap.put(k, count);
+                } else {
+                    countMap.put(k, 0);
+                }
+            }
+            rowNum++;
+        }
+        return countMap;
+    }
+
+    private static Map<String, Integer> countKanaMap(Map<String, Integer> map) {
+        Map<String, Integer> countKanaMap = new LinkedHashMap<>();
+        for (KanaType kanaType : KanaType.values()) {
+            for (int i = 0; i < kanaType.getKana().length; i++) {
+                String key;
+                int count = 0;
+
+                String kana = kanaType.getKana()[i];
+                if (kana.isEmpty()) continue;
+                if (map.containsKey(kana)) count = map.get(kana);
+                key = kana;
+
+                if (kanaType.getSecondKana() != null) {
+                    String secondKana = kanaType.getSecondKana()[i];
+                    if (map.containsKey(secondKana)) {
+                        count += map.get(secondKana);
+                        key += ", " + secondKana;
+                    }
+                }
+                if (kanaType.getThirdKana() != null) {
+                    String thirdKana = kanaType.getThirdKana()[i];
+                    if (map.containsKey(thirdKana)) {
+                        count += map.get(thirdKana);
+                        key += ", " + thirdKana;
+                    }
+                }
+                countKanaMap.put(key, count);
+            }
+        }
+        return countKanaMap;
+    }
 }
